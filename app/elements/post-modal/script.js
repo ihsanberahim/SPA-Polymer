df0dc4c01bc69b76eaa0629990c7b93dd11039e9
Muhammad Nur'Ihsan Berahim <ihsanberahim@gmail.com>
2015-04-10 05:36:17 +0800

    remove another project elements. update post-card and post list.

--- app/elements/post-modal/script.js
+++ app/elements/post-modal/script.js
@@ -5,25 +5,42 @@
       sLeft: 0,
       sHeight: 0,
       sWidth: 0,
-      screen_title: 'Post'
+      screen_title: 'Post',
+      animation: '', /*scale | ripple*/
+      ANIMATION_SCALE: 'scale',
+      ANIMATION_RIPPLE: 'ripple'
     },
     attached: function()
     {
+      if(this.animation=='') this.animation = this.ANIMATION_SCALE;
+
       if(window.device && window.device.platform=='iOS')
       {
         this.$.toolbar.style.paddingTop = '8pt';
+      }
+      
+      if(this.animation==this.ANIMATION_SCALE)
+      {
+        TweenMax.set(this, { top: this.sTop, left: this.sLeft, height: this.sHeight, width: this.sWidth });
       }
       
       this.show();
     },
     show: function(){
       this.style.zIndex = xdCount();
-      TweenMax.to(this, 0.35, { top: 0, left: 0, height: '100%', width: '100%' });
-      TweenMax.to(this.$.content, 0.35, { opacity: 1, delay: 0.35 });
+
+      if(this.animation==this.ANIMATION_SCALE)
+      {
+        TweenMax.to(this, 0.35, { top: 0, left: 0, height: '100%', width: '100%' });
+        TweenMax.to(this.$.content, 0.35, { opacity: 1, delay: 0.35 });
+      }
     },
     close: function(){
-      TweenMax.to(this.$.content, 0.25, { opacity: 0 });
-      TweenMax.to(this, 0.35, { top: this.sTop, left: this.sLeft, height: this.sHeight, width: this.sWidth, delay: 0.35, onComplete: this.destroy.bind(this) });
+      if(this.animation==this.ANIMATION_SCALE)
+      {
+        TweenMax.to(this.$.content, 0.25, { opacity: 0 });
+        TweenMax.to(this, 0.35, { top: this.sTop, left: this.sLeft, height: this.sHeight, width: this.sWidth, delay: 0.35, onComplete: this.destroy.bind(this) });
+      }
     },
     destroy: function()
     {
